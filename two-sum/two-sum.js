a9773dde4f7d01806b02c9f450e5630327c819fe
Kenneth Lee <95536900+kennethlee0502@users.noreply.github.com>
2023-04-18 10:59:36 -0400

    Time: 76 ms (59.82%) | Memory: 42.7 MB (49.78%) - LeetSync

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -4,14 +4,13 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-const map = new Map();
-
-for(let i = 0;i< nums.length;i++){
-    const complement = target - nums[i];
-    if(map.has(complement)){
-        return [map.get(complement),i];
-    }
-    map.set(nums[i],i)
+let map = new Map()
+for(let i = 0; i < nums.length; i ++){
+const minus = target - nums[i];
+if(map.has(minus)){
+    return [map.get(minus),i]
+}
+map.set(nums[i],i)
 }
 return []
 }
