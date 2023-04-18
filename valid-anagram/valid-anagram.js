4bb0e808715c37a5ba32d6283b7693403649b33c
Kenneth Lee <95536900+kennethlee0502@users.noreply.github.com>
2023-04-18 10:27:28 -0400

    Time: 89 ms (47.47%) | Memory: 44.3 MB (52.06%) - LeetSync

--- valid-anagram/valid-anagram.js
+++ valid-anagram/valid-anagram.js
@@ -7,18 +7,17 @@ var isAnagram = function(s, t) {
 if(s.length !== t.length){
     return false
 }
-let frequency = {};
+let result = {}
 for(let char of s){
-    frequency[char] = frequency[char] ? frequency[char] +1 : 1;
+    result[char] = result[char] ? result[char] + 1 : 1;
 }
-for (let char of t){
-    if(!frequency[char]){
+for(let char of t){
+    if(!result[char]){
         return false
-    }
-    frequency[char] --
+    } result[char] --
 }
-for ( let char in frequency){
-    if(frequency[char]!== 0){
+for(let char in result){
+    if(result[char] !== 0){
         return false
     }
 }
